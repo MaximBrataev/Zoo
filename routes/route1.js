a8631b8bc7f8fc88109a702df2364139b56dc6c9
MaxBrat <privetovscky@gmail.com>
2023-03-08 18:26:33 +0300

    first commit

---
+++ routes/route1.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const Layout = require('../components/Layout');
+const renderComponent = require('../middleware/renderComponent');
+
+router.get('/', async (req, res) => {
+  try {
+    // const students = await Student.findAll({ raw: true });
+    // console.log(req.cookies);
+    res.renderComponent(Layout, { title: 'Зоопарк' });
+  } catch (error) {
+    res.send(console.log(error.message));
+  }
+});
+
+module.exports = router;
